de7eb552aafdfbb5366b729cf6db75d0199d046d
nyxrotten <nes_calina@hotmail.com>
2024-06-17 12:46:31 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+
 
 function App() {
- //Aquí tu código
+
+    const [name, setName] = useState("Sofía");
+
+    return(
+        <div>
+        <h2>Teacher name: {name}</h2>
+            <ul>
+                <li onClick={() => setName("Data")}><p>Data</p></li>
+                <li onClick={() => setName("Reyes")}><p>Reyes</p></li>
+                <li onClick={() => setName("Yolanda")}><p>Yolanda</p></li>
+            </ul>
+    </div>
+    )
 }
 
 export default App
