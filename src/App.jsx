931e19667afd75b89c681720fc56b6dc48637d74
nyxrotten <nes_calina@hotmail.com>
2024-06-18 16:05:33 +0200

    first part done - bonus incomplete

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,41 @@
 import { useState } from 'react';
 import './App.css';
 
+// Ejercicio
+// function App() {
+
+//     const [name, setName] = useState("Sofía");
+
+//     return(
+//         <div>
+//         <h2>Teacher name: {name}</h2>
+//             <ul>
+//                 <li onClick={() => setName("Data")}><p>Data</p></li>
+//                 <li onClick={() => setName("Reyes")}><p>Reyes</p></li>
+//                 <li onClick={() => setName("Yolanda")}><p>Yolanda</p></li>
+//             </ul>
+//     </div>
+//     )
+// }
+
+//Bonus
 
 function App() {
 
-    const [name, setName] = useState("Sofía");
+    const [name, setName] = useState("");
 
+    
     return(
         <div>
+            <form>
+                <input type="text"
+                value={name}
+                onChange={e => setName(e.target.value)}
+                placeholder="add a name"/>
+                <button onClick={() => setName(target.value)}>Add</button>
+            </form>
         <h2>Teacher name: {name}</h2>
-            <ul>
-                <li onClick={() => setName("Data")}><p>Data</p></li>
-                <li onClick={() => setName("Reyes")}><p>Reyes</p></li>
-                <li onClick={() => setName("Yolanda")}><p>Yolanda</p></li>
-            </ul>
     </div>
     )
 }
-
 export default App
